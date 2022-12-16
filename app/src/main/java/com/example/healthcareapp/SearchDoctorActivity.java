8cfaa0107862ac9af493d17fb51b624bc375d19a
Sadia Afrin <sadiaafrin@Sadias-MacBook-Pro.local>
2022-12-17 02:23:19 +0600

    search doctor completed fxml

--- app/src/main/java/com/example/healthcareapp/SearchDoctorActivity.java
+++ app/src/main/java/com/example/healthcareapp/SearchDoctorActivity.java
@@ -4,7 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class Search_Doctor extends AppCompatActivity {
+public class SearchDoctorActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
