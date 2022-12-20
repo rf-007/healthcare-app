d7238388b987502b9df6b7c336b7dbe76500d544
ARAFRIN49784 <afrinsadia682@gmail.com>
2022-12-20 21:01:59 +0600

    bookappointment activity created

--- app/src/main/java/com/example/healthcareapp/BookAppointment.java
+++ app/src/main/java/com/example/healthcareapp/BookAppointment.java
@@ -1,13 +1,24 @@
 package com.example.healthcareapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
 import android.os.Bundle;
+
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
 
 import com.example.healthcareapp.databinding.ActivityBookAppointmentBinding;
 
 public class BookAppointment extends AppCompatActivity {
     ActivityBookAppointmentBinding activityBookAppointmentBinding;
     AptDBHelper aptDBHelper;
+    EditText address,phoneNumber,fullName,date,time;
+    Button button_registration;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,5 +27,65 @@ public class BookAppointment extends AppCompatActivity {
         setContentView(activityBookAppointmentBinding.getRoot());
 
 
+        fullName = (EditText) findViewById(R.id.fullName);
+        phoneNumber = (EditText) findViewById(R.id.phoneNumber);
+        address = (EditText) findViewById(R.id.address);
+        date = (EditText) findViewById(R.id.date);
+        time = (EditText) findViewById(R.id.time);
+
+        button_registration = (Button) findViewById(R.id.button_registration);
+        aptDBHelper =  new AptDBHelper (this);
+
+        button_registration.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View view) {
+                String name = fullName.getText().toString();
+                String phone = phoneNumber.getText().toString();
+                String email = address.getText().toString();
+                String Dob = date.getText().toString();
+                String Tn = time.getText().toString();
+
+
+                if(name.equals("") || phone.equals("") || email.equals("") || Dob.equals("") || Tn.equals("")) {
+                    Toast.makeText(BookAppointment.this, "Enter all the values in the field", Toast.LENGTH_SHORT).show();
+                }
+
+
+                else{
+                    Boolean ress = aptDBHelper.aptverify(name);
+                    if(ress == false)
+                    {
+                        Boolean res = aptDBHelper.insertAppointment(name,phone,email,Dob,Tn);
+                        if(res==true)
+                        {
+                            Toast.makeText(BookAppointment.this,"Booking appointment Successful!", Toast.LENGTH_SHORT).show();
+                            Intent intent = new Intent(getApplicationContext(), HomePageActivity.class);
+                            startActivity(intent);
+                        }
+                        else
+                        {
+                            Toast.makeText(BookAppointment.this,"Sorry, BookAppointment Failed. Please Try Again", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                    else{
+
+                        Intent intent = new Intent(getApplicationContext(), HomePageActivity.class);
+                        startActivity(intent);
+                    }
+                }
+            }
+
+
+
+
+        });
+
+
+
+
     }
 }
+
+
+
